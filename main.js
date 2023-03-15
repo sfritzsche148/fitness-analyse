fe68084b735b06be9b4e18a87453c94dd16c6782
Sebastian Fritzsche <sebifr58@gmx.de>
2023-03-15 19:23:19 +0100

    schritte anzeigen lassen

--- main.js
+++ main.js
@@ -5,11 +5,27 @@ passbtn.addEventListener('click', function() {
 
     let passinp = document.getElementById('passwordinput').value;
     let login = document.getElementById('login');
+    let content = document.getElementById('content');
 
     if (passinp === 'sfritzsche') {
-        alert('Login');
-        login.style.display = 'none'
+        login.style.display = 'none';
+        content.style.display = 'block';
     }else {
-        alert('Wrong Password')
+        alert('Wrong Password');
     }
 })
+
+// Schritte tabelle
+function Schritte() {
+    let schrittetable = document.getElementById('schrittetable');
+    let p = document.createElement('p');
+    let schrittcount = document.getElementById('schrittcount').value;
+
+    let d = new Date().getDate();
+    let month = new Date().getMonth();
+    let year = new Date().getFullYear();
+
+    p.innerHTML = d + '.'+ month + '.' + year + ' ' + schrittcount;
+
+    schrittetable.appendChild(p);
+}
